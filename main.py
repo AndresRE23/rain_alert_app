46e9c383cab632057861d5b4dfe879d6cb6fcde1
Andrés <andresre@icloud.com>
2025-09-04 00:52:33 -0600

    refactor: adjust code logic for clarity

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ load_dotenv()
 OMW_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = os.getenv("API_KEY")
 
-condition_list = []
-
 parameters = {
     "lat": 9.915488,
     "lon": -83.999890,
@@ -20,13 +18,15 @@ response = requests.get(OMW_endpoint, params=parameters)
 response.raise_for_status()
 data = response.json()
 
-for i in range(0, parameters["cnt"]):
-    condition = data["list"][i]["weather"][0]["id"]
-    condition_list.append(condition)
+will_rain = False
 
-for condition in condition_list:
-    if condition < 700:
-        print("Hoy hay que llevar sombrilla.")
+for hour_data in data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Hoy probablemente va a llover.")
 
 
 
